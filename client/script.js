12ebb8e8c4d1d083fb8e1c8183335c9d468d87ad
Pedro Gaspar <ordep12@hotmail.com.br>
2023-03-31 13:18:25 -0300

    Loader, typeText and uniqueId add.

--- client/script.js
+++ client/script.js
@@ -1,24 +1,41 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
 
-setupCounter(document.querySelector('#counter'))
+let loadInterval;
+
+
+function loader(element){
+  element.textContext = '';
+
+  loadInterval = setInterval(() => {
+    element.textContext += '.';
+
+    if(element.textContext === '....'){//whit if makes the '...' of the AI before he gives an answer
+      element.textContext = '';
+    }
+  }, 300)// 300miliseconds per .
+}
+
+function typeText(element, text){
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if(index < text.length){
+      element.innerHTML += text.chartAt(index);
+    }else{
+      clearInterval(interval);
+    }
+  }, 20)//20 miliseconds per letter
+}
+
+function generateUniqueId(){
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`//this generates an unique random ID with the current time and random number.
+}
+
